69558b34873addb70c14f98159ac3791add8c29b
H0rn3t Sp1d3rs <97798085+H0rn3t-Sp1d3rs@users.noreply.github.com>
2022-12-04 20:38:39 +0600

    Create Api.py

---
+++ Api.py
@@ -0,0 +1,10 @@
+import requests
+
+n = input("Enter Your Number: ")
+a = int(input ("Enter Your Amount: "))
+
+
+for i in range (a):
+	r= requests.get("http://deepitsolution.in/api.php?number="+n)
+	f= r.content
+	print(f)
